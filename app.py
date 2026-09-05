ca2af18d5a7c55d14bab049e448ad2735ce1a885
Loise <321818292+loisenordbotten@users.noreply.github.com>
2026-09-05 16:53:55 +0200

    flask-app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<1>Hei fra Flask!<\h1><p>hei hei hei</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
