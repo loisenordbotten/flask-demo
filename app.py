5ef407e7c039b8ad6101cf821a6f4e5da1b7650c
Loise <321818292+loisenordbotten@users.noreply.github.com>
2026-09-05 16:58:19 +0200

    endret return-linja

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<1>Hei fra Flask!<\h1><p>hei hei hei</p>"
+    return "<1>Hei fra Flask!<\h1><p>heisann hoppsann</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
