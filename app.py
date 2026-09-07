3b097d9d49afa16eed0b52a30fb124e1b3595c32
Loise <321818292+loisenordbotten@users.noreply.github.com>
2026-09-07 20:34:59 +0200

    la til docker og miljøvariabler

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv()
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<1>Hei fra Flask!<\h1><p>heisann hoppsann</p>"
+    hemmelighet = os.getenv("SECRET_MESSAGE", "ingen hemmelighet satt")
+    return f"<h1>Hei fra Flask!</h1><p>{hemmelighet}</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
